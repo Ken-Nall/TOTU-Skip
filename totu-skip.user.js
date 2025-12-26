e6c587fc78491aae2e1d4c2cdefbc68711cb3917
Kenneth Nall <ken.nall.strive@gmail.com>
2025-12-26 14:05:04 -0600

    Initial gen. Working.

---
+++ totu-skip.user.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name         TOTU Skip
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Automatically close Time Off Task Updated modal
+// @author       Ken-Nall
+// @match        https://fclm-portal.amazon.com/employee*
+// @updateURL    https://raw.githubusercontent.com/Ken-Nall/TOTU-Skip/main/totu-skip.user.js
+// @downloadURL  https://raw.githubusercontent.com/Ken-Nall/TOTU-Skip/main/totu-skip.user.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Observer to watch for modal appearance
+    const observer = new MutationObserver((mutations) => {
+        mutations.forEach((mutation) => {
+            mutation.addedNodes.forEach((node) => {
+                if (node.nodeType === 1 && node.classList && node.classList.contains('ui-dialog')) {
+                    const title = node.querySelector('.ui-dialog-title');
+                    if (title && title.textContent.trim() === 'Time Off Task Updated') {
+                        const okButton = node.querySelector('.ui-dialog-buttonset button');
+                        if (okButton) {
+                            okButton.click();
+                        }
+                    }
+                }
+            });
+        });
+    });
+
+    // Start observing
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true
+    });
+})();
